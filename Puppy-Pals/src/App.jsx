839790e8e144fc1d9dbdf2c1a18e0e1e14b590fd
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-07 15:04:16 -0400

    added new codes

--- Puppy-Pals/src/App.jsx
+++ Puppy-Pals/src/App.jsx
@@ -1,27 +1,21 @@
+import {puppyList} from './data.js'
 import { useState } from 'react'
 
-import './App.css'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      
-      <h1>Puppy Pals</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
-
 export default App
