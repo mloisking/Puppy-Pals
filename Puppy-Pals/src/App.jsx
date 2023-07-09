454e218356816d6af12904f378e0e07ddadb063e
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-09 18:48:16 -0400

    Added new codes to the App.jsx

--- Puppy-Pals/src/App.jsx
+++ Puppy-Pals/src/App.jsx
@@ -1,21 +1,31 @@
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
 import { useState } from 'react'
 
-
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  <button onClick={()=>{ setFeatPupId(puppy.id)}}></button>
 
-  console.log("puppyList: ", puppyList);
+  function handleClick() {
+ return(
+ <button onClick={()=>{console.log("puppy id: ", puppy.id)}}></button>
+ )};
+ 
+ return (
+  <div className="App">
+    {puppies.map((puppy) => {
+      return (
+        <p onClick={handleClick} key={puppy.id}>
+          {puppy.name}
+        </p>
+      );
+    })}
+    <p>{featPupId}</p>
+  </div>
+);
+}       
+           
+      
+ 
 
-  return (
-    <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    </div>
-  );
-}
 export default App
