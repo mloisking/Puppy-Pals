31c954d6b89ccf04b1ca0ac67f6a1e10ae596b84
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-09 21:00:31 -0400

    added new codes to App.jsx

--- Puppy-Pals/src/App.jsx
+++ Puppy-Pals/src/App.jsx
@@ -2,30 +2,39 @@ import { puppyList } from './data.js'
 import { useState } from 'react'
 
 function App() {
+  <h2>Puppy Pals</h2>
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  <button onClick={()=>{ setFeatPupId(puppy.id)}}></button>
 
   function handleClick() {
- return(
- <button onClick={()=>{console.log("puppy id: ", puppy.id)}}></button>
- )};
- 
- return (
-  <div className="App">
-    {puppies.map((puppy) => {
-      return (
-        <p onClick={handleClick} key={puppy.id}>
-          {puppy.name}
-        </p>
-      );
-    })}
-    <p>{featPupId}</p>
-  </div>
-);
-}       
-           
-      
- 
+    return (
+      <button onClick={() => { console.log("puppy id: ", puppy.id) }}></button>
+    )
+  };
+  <button onClick={() => { setFeatPupId(puppy.id) }}></button>;
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name} {puppy.isCute}
+            <img src="puppy.jpeg" width="150" height="200"></img>
+            <button>Click Me</button>
+
+          </p>
+        );
+      }
+      )};
+      <p>{featPupId && <p>{featPupId}</p>}</p>
+    </div>
+  )
+};
+
+{
+  
+}
 
 export default App
