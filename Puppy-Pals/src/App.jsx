215e6f8bcdbb87dda44aef6eb675642498fc9ef2
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-16 10:59:09 -0400

    added new codes to App.jsx and data.js

--- Puppy-Pals/src/App.jsx
+++ Puppy-Pals/src/App.jsx
@@ -2,40 +2,37 @@ import { puppyList } from './data.js'
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
-  <div>
-    <h2>Puppy Pals</h2>
-  </div>
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick() {
-    return (
-      <button onClick={() => { console.log("puppy id: ", puppy.id) }}></button>
-    )
+  function handleClick(id) {
+    setFeatPupId(id)
   };
-  <button onClick={() => { setFeatPupId(puppy.id) }}></button>;
+ 
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup);
   return (
     <div className="App">
+        <div>
+    <h2>Puppy Pals</h2>
+  </div>
       {puppies.map((puppy) => {
         return (
-          <p onClick={handleClick} key={puppy.id}>
-            {puppy.name} {puppy.isCute} {puppy.img}
+          <div key={puppy.id}>
+            {puppy.name} {puppy.isCute} 
             <img 
             className="Puppy"
-            src="puppy.jpeg" width="150" height="200"></img>
-            <button>Click Me</button>
-            </p>
+            src={puppy.puppyimage} width="150" height="200"></img>
+           <button onClick={() => { handleClick(puppy.id) }}>click me</button>;
+            </div>
         );
       })}
-      <p>{ featPupId && <p>{ featPupId }</p> } </p>
+      { featPupId && <p>{ featPupId }</p> }
     </div>
   );
     };
 
 
 export default App
-
-  
--- Puppy-Pals/src/data.js
+++ Puppy-Pals/src/data.js
@@ -1,68 +1,83 @@
+import puppyimage from './puppypicture.jpeg'
+import puppyimage1 from './cutepup.jpeg'
+import puppyimage2 from './cutepup2.jpeg'
+import puppyimage3 from './brownpuppy.jpeg'
+import puppyimage4 from './Daschundpuppy.jpeg'
+import puppyimage5 from './PlayfulPup.jpeg'
+import puppyimage6 from './PlayfulPup2.jpeg'
+
 export const puppyList = [
-    {
-      id: 1,
-      name: "Sir Waggington",
-      email: "sir-wag@email.com",
-      isCute: true,
-      age: 5,
-      ownerId: 1,
-      tricks: [
-        { id: 2, title: "Lay Down" },
-        { id: 3, title: "Jump through flaming hoop" },
-      ],
-    },
-    {
-      id: 2,
-      name: "Fiona Penny Pickles",
-      email: "pick-your-pennies@email.com",
-      isCute: true,
-      age: 6,
-      ownerId: 1,
-      tricks: [],
-    },
-    {
-      id: 3,
-      name: "Professor Wagglesworth",
-      email: "waggie@email.com",
-      isCute: true,
-      age: 6,
-      ownerId: 2,
-      tricks: [{ id: 1, title: "Sit" }],
-    },
-    {
-      id: 4,
-      name: "Sergeant Barkowitz",
-      email: "the-sarge@email.com",
-      isCute: true,
-      age: 4,
-      ownerId: 2,
-      tricks: [{ id: 2, title: "Lay Down" }],
-    },
-    {
-      id: 5,
-      name: "Captain Sniffer",
-      email: "capn-sniff@email.com",
-      isCute: true,
-      age: 7,
-      ownerId: 3,
-      tricks: [],
-    },
-    {
-      id: 6,
-      name: "Miss Furbulous",
-      email: "miss-furby@email.com",
-      isCute: true,
-      age: 1,
-      ownerId: 3,
-      tricks: [],
-    },
-    {
-      id: 7,
-      name: "Alfred von Wigglebottom",
-      email: "alfie@email.com",
-      isCute: true,
-      age: 2,
-      ownerId: 3,
-      tricks: [],
-    },
-  ];
+  {
+    id: 1,
+    name: "Sir Waggington",
+    email: "sir-wag@email.com",
+    isCute: true,
+    age: 5,
+    ownerId: 1,
+    tricks: [
+      { id: 2, title: "Lay Down" },
+      { id: 3, title: "Jump through flaming hoop" },
+    ],
+    puppyimage: puppyimage
+  },
+  {
+    id: 2,
+    name: "Fiona Penny Pickles",
+    email: "pick-your-pennies@email.com",
+    isCute: true,
+    age: 6,
+    ownerId: 1,
+    tricks: [],
+    puppyimage: puppyimage1
+  },
+  {
+    id: 3,
+    name: "Professor Wagglesworth",
+    email: "waggie@email.com",
+    isCute: true,
+    age: 6,
+    ownerId: 2,
+    tricks: [{ id: 1, title: "Sit" }],
+   puppyimage: puppyimage2
+  },
+  {
+    id: 4,
+    name: "Sergeant Barkowitz",
+    email: "the-sarge@email.com",
+    isCute: true,
+    age: 4,
+    ownerId: 2,
+    tricks: [{ id: 2, title: "Lay Down" }],
+  puppyimage: puppyimage3
+  },
+  {
+    id: 5,
+    name: "Captain Sniffer",
+    email: "capn-sniff@email.com",
+    isCute: true,
+    age: 7,
+    ownerId: 3,
+    tricks: [],
+    puppyimage: puppyimage4
+  },
+  {
+    id: 6,
+    name: "Miss Furbulous",
+    email: "miss-furby@email.com",
+    isCute: true,
+    age: 1,
+    ownerId: 3,
+    tricks: [],
+    puppyimage:puppyimage5
+  },
+  {
+    id: 7,
+    name: "Alfred von Wigglebottom",
+    email: "alfie@email.com",
+    isCute: true,
+    age: 2,
+    ownerId: 3,
+    tricks: [],
+    puppyimage: puppyimage6
+  },
+];
